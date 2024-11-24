8750aa594143efb38ad132b2f0b93795f7edfe99
lukchau <lukagogokhiya1988@gmail.com>
2024-11-24 20:29:30 +0300

    зум + косметические изменения

--- src/app.py
+++ src/app.py
@@ -1,9 +1,8 @@
 import sys
 import random
-from PyQt5.QtWidgets import QMainWindow, QApplication, QShortcut, QMenuBar, QAction, QInputDialog, QColorDialog, QFileDialog
-from PyQt5.QtGui import QPainter, QPen, QKeySequence
-from PyQt5.QtCore import Qt, QPoint
-
+from PyQt5.QtWidgets import QMainWindow, QApplication, QShortcut, QToolBar, QAction, QInputDialog, QColorDialog, QFileDialog, QVBoxLayout, QWidget, QLabel, QSpinBox, QHBoxLayout
+from PyQt5.QtGui import QPainter, QPen, QKeySequence, QImage, QPixmap
+from PyQt5.QtCore import Qt, QPoint, QRect, QSize
 
 class PaintWidget(QMainWindow):
     """
@@ -16,13 +15,17 @@ class PaintWidget(QMainWindow):
         Создает список линий и устанавливает флаг рисования в False
         """
         super().__init__()
-        self.menubar = MenuBar(self)
-        self.setMenuBar(self.menubar)
 
-        self.lines_buffer = []  
-        self.drawing = False  
+        self.setWindowTitle("Paintmaxxing")
+        self.setStyleSheet("QMainWindow { background-color: #f0f0f0; }")
+
+        self.toolbar = QToolBar(self)
+        self.addToolBar(Qt.TopToolBarArea, self.toolbar)
+
+        self.lines_buffer = []
+        self.drawing = False
         self.undo_stack = [[]]
-        self.redo_stack = []  
+        self.redo_stack = []
 
         self.current_pen = QPen(Qt.black, 2, Qt.SolidLine)
         self.current_tool = "Линия"  # Дифолтный инструмент
@@ -34,28 +37,92 @@ class PaintWidget(QMainWindow):
         self.redo_shortcut = QShortcut(QKeySequence("Ctrl+Y"), self)
         self.redo_shortcut.activated.connect(self.redo)
 
+        self.zoom_level = 100
+        self.zoom_spinbox = QSpinBox()
+        self.zoom_spinbox.setRange(10, 500)
+        self.zoom_spinbox.setValue(self.zoom_level)
+        self.zoom_spinbox.valueChanged.connect(self.on_zoom_changed)
+
+        self.offset = QPoint(0, 0)
+        self.panning = False
+        self.pan_start = QPoint(0, 0)
+
+        self.sheet_size = QSize(self.width() * 2, (self.height() - self.toolbar.height()) * 2)
+        self.original_offset = QPoint(0, 0)
+
+        self.init_toolbar()
+        self.init_zoom_control()
+
+    def init_toolbar(self):
+        self.line_tool = QAction("Линия", self)
+        self.graffiti_tool = QAction("Граффити", self)
+        self.color_action = QAction("Цвет", self)
+        self.thickness_action = QAction("Толщина", self)
+        self.save_as_jpg_action = QAction("Сохранить как JPG", self)
+        self.save_as_png_action = QAction("Сохранить как PNG", self)
+        self.undo_action = QAction("Отменить", self)
+        self.redo_action = QAction("Повторить", self)
+
+        self.toolbar.addAction(self.line_tool)
+        self.toolbar.addAction(self.graffiti_tool)
+        self.toolbar.addAction(self.color_action)
+        self.toolbar.addAction(self.thickness_action)
+        self.toolbar.addAction(self.save_as_jpg_action)
+        self.toolbar.addAction(self.save_as_png_action)
+        self.toolbar.addAction(self.undo_action)
+        self.toolbar.addAction(self.redo_action)
+
+        self.line_tool.triggered.connect(self.on_line_tool_triggered)
+        self.graffiti_tool.triggered.connect(self.on_graffiti_tool_triggered)
+        self.color_action.triggered.connect(self.on_color_menu_triggered)
+        self.thickness_action.triggered.connect(self.on_thickness_menu_triggered)
+        self.save_as_jpg_action.triggered.connect(self.on_save_as_jpg_triggered)
+        self.save_as_png_action.triggered.connect(self.on_save_as_png_triggered)
+        self.undo_action.triggered.connect(self.undo)
+        self.redo_action.triggered.connect(self.redo)
+
+    def init_zoom_control(self):
+        zoom_label = QLabel("Зум:")
+        zoom_layout = QHBoxLayout()
+        zoom_layout.addWidget(zoom_label)
+        zoom_layout.addWidget(self.zoom_spinbox)
+
+        zoom_widget = QWidget()
+        zoom_widget.setLayout(zoom_layout)
+        zoom_widget.setFixedWidth(150)
+
+        self.statusBar().addPermanentWidget(zoom_widget)
+
     def mousePressEvent(self, event):
         """
         Обработка события нажатия левой кнопки мыши
         Начинает новую линию, если нажата левая кнопка мыши
         """
-        if event.button() == Qt.LeftButton:
+        if event.button() == Qt.LeftButton and event.y() > self.toolbar.height():
             # Новый QPen с текущими настройками
             new_pen = QPen(self.current_pen.color(), self.current_pen.width(), self.current_pen.style())
-            self.lines_buffer.append(([], new_pen))  # Новая линия 
-            self.drawing = True  
-            self.update()  
+            self.lines_buffer.append(([], new_pen))  # Новая линия
+            self.drawing = True
+            self.update()
+        elif event.button() == Qt.RightButton:
+            self.panning = True
+            self.pan_start = event.pos()
 
     def mouseMoveEvent(self, event):
         """
         Обработка события движения мыши
         Добавляет текущую позицию мыши в линию, когда пользователь рисует
         """
-        if self.drawing:
+        if self.drawing and event.y() > self.toolbar.height():
             if self.current_tool == "Линия":
-                self.lines_buffer[-1][0].append(event.pos())  # Добавление точки в линию
+                self.lines_buffer[-1][0].append(self.adjust_mouse_position(event.pos()))  # Добавление точки в линию
             elif self.current_tool == "Граффити":
-                self.spray(event.pos())
+                self.spray(self.adjust_mouse_position(event.pos()))
+            self.update()
+        elif self.panning:
+            delta = event.pos() - self.pan_start
+            self.offset += delta
+            self.pan_start = event.pos()
             self.update()
 
     def mouseReleaseEvent(self, event):
@@ -63,10 +130,12 @@ class PaintWidget(QMainWindow):
         Обработка события отпускания мыши
         Завершает рисование, если была отпущена левая кнопка мыши
         """
-        if event.button() == Qt.LeftButton:
+        if event.button() == Qt.LeftButton and event.y() > self.toolbar.height():
             self.drawing = False
             self.undo_stack.append(self.lines_buffer[:])  # Сохраняем текущее состояние для отмены
             self.redo_stack.clear()  # Очищаем стек повтора при новом действии
+        elif event.button() == Qt.RightButton:
+            self.panning = False
 
     def paintEvent(self, event):
         """
@@ -74,6 +143,12 @@ class PaintWidget(QMainWindow):
         Отрисовка линий на основе точек
         """
         qp = QPainter(self)
+        qp.fillRect(self.rect(), Qt.lightGray)  # Заполнить фон серым цветом
+        qp.translate(self.offset)
+        qp.scale(self.zoom_level / 100.0, self.zoom_level / 100.0)
+        qp.fillRect(QRect(QPoint(0, 0), self.sheet_size), Qt.white)  # Заполнить лист белым цветом
+        drawing_area = QRect(0, 0, self.sheet_size.width(), self.sheet_size.height())
+        qp.setClipRect(drawing_area)
         for line, pen in self.lines_buffer:
             qp.setPen(pen)  # Текущие настройки
             for i in range(len(line) - 1):
@@ -81,7 +156,6 @@ class PaintWidget(QMainWindow):
 
         qp.end()
 
-        
     def spray(self, position):
         """
         Симуляция эффекта граффити путем рисования случайных точек вокруг курсора
@@ -98,11 +172,11 @@ class PaintWidget(QMainWindow):
     def undo(self):
         """
         Отмена последнего действия
-        """ 
+        """
         if self.undo_stack:
             self.redo_stack.append(self.lines_buffer[:])  # Сохраняем текущее состояние для повтора
             self.lines_buffer = self.undo_stack.pop()  # Восстанавливаем предыдущее состояние
-            self.update()  
+            self.update()
 
     def redo(self):
         """
@@ -117,117 +191,73 @@ class PaintWidget(QMainWindow):
         """
         Обработка события изменения размера окна
         """
-        # Изменение размера в соответствии с размером окна
-        width_scale = event.size().width() / self.size().width()
-        height_scale = event.size().height() / self.size().height()
-
-        self.lines_buffer = [
-            ([QPoint(int(p.x() * width_scale), int(p.y() * height_scale)) for p in line[0]], line[1])
-            for line in self.lines_buffer
-        ]
-
         super().resizeEvent(event)
-    
-    def save_image(self, file_name, format, exclude_menu=False):
-        if exclude_menu:
-            drawing_area = self.geometry()
-            drawing_area.setTop(drawing_area.top() + self.menubar.height())  
+        self.sheet_size = QSize(self.width() * 2, (self.height() - self.toolbar.height()) * 2)
 
-            image = self.grab(drawing_area)
-        else:
-            image = self.grab(Qt.CopyAllAttributes)  
+    def save_image(self, file_name, format):
+        # Сохранить полное изображение в файл
+        drawing_area = QRect(0, 0, self.sheet_size.width(), self.sheet_size.height())
+        image = QImage(drawing_area.size(), QImage.Format_ARGB32)
+        image.fill(Qt.white)
 
+        qp = QPainter(image)
+        for line, pen in self.lines_buffer:
+            qp.setPen(pen)
+            for i in range(len(line) - 1):
+                qp.drawLine(line[i], line[i + 1])
+
+        qp.end()
         image.save(file_name, format)
 
-
-class ToolAction(QAction):
-    def __init__(self, name, parent=None):
-        super().__init__(name, parent)
-        self.name = name
-
-class LineTool(ToolAction):
-    def __init__(self, parent=None):
-        super().__init__("Линия", parent)
-
-class GraffitiTool(ToolAction):
-    def __init__(self, parent=None):
-        super().__init__("Граффити", parent)
-
-class MenuBar(QMenuBar):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-        self.file_menu = self.addMenu("Файл")
-        self.save_as_menu = self.file_menu.addMenu("Сохранить как")
-        self.save_as_jpg_action = QAction("JPG", self)
-        self.save_as_png_action = QAction("PNG", self)
-        self.save_as_menu.addAction(self.save_as_jpg_action)
-        self.save_as_menu.addAction(self.save_as_png_action)
-
-        self.tools_menu = self.addMenu("Инструменты")
-        self.eraser_menu = self.addMenu("Ластик")
-        self.figures_menu = self.addMenu("Фигуры")
-        # TODO: добавить фигуры (круг, квадрат, треугольник)
-        self.color_thickness_menu = self.addMenu("Цвет и Толщина")
-        # TODO: добавить выбор цвета и толщины
-
-        self.line_tool = LineTool(self)
-        self.graffiti_tool = GraffitiTool(self)
-
-        self.tools_menu.addAction(self.line_tool)
-        self.tools_menu.addAction(self.graffiti_tool)
-
-        self.line_tool.triggered.connect(self.on_line_tool_triggered)
-        self.graffiti_tool.triggered.connect(self.on_graffiti_tool_triggered)
-
-        self.color_action = QAction("Цвет", self)
-        self.color_action.triggered.connect(self.on_color_menu_triggered)
-        self.color_thickness_menu.addAction(self.color_action)
-
-        self.thickness_action = QAction("Толщина", self)
-        self.thickness_action.triggered.connect(self.on_thickness_menu_triggered)
-        self.color_thickness_menu.addAction(self.thickness_action)
-
-        self.save_as_jpg_action.triggered.connect(self.on_save_as_jpg_triggered)
-        self.save_as_png_action.triggered.connect(self.on_save_as_png_triggered)
-
-
     def on_line_tool_triggered(self):
-        self.parent().current_tool = "Линия"
-        self.parent().current_pen = QPen(self.parent().current_pen.color(), self.parent().current_pen.width(), Qt.SolidLine) 
+        self.current_tool = "Линия"
+        self.current_pen = QPen(self.current_pen.color(), self.current_pen.width(), Qt.SolidLine)
 
     def on_graffiti_tool_triggered(self):
-        self.parent().current_tool = "Граффити"
-        self.parent().current_pen = QPen(self.parent().current_pen.color(), self.parent().current_pen.width(), Qt.SolidLine)
-        self.parent().drawing = True 
-        self.parent().update() 
-    
+        self.current_tool = "Граффити"
+        self.current_pen = QPen(self.current_pen.color(), self.current_pen.width(), Qt.SolidLine)
+        self.drawing = True
+        self.update()
+
     def on_color_menu_triggered(self):
         color = QColorDialog.getColor()
         if color.isValid():
-            self.parent().current_pen.setColor(color)
+            self.current_pen.setColor(color)
 
     def on_thickness_menu_triggered(self):
         thickness, ok = QInputDialog.getInt(self, "Толщина", "Введите толщину:")
         if ok:
-            self.parent().current_pen.setWidth(thickness)
+            self.current_pen.setWidth(thickness)
 
     def on_save_as_jpg_triggered(self):
         file_name, _ = QFileDialog.getSaveFileName(self, "Сохранить как", "", "JPG Файл (*.jpg)")
         if file_name:
-            self.parent().save_image(file_name, "jpg", exclude_menu=True)
+            self.save_image(file_name, "jpg")
 
     def on_save_as_png_triggered(self):
         file_name, _ = QFileDialog.getSaveFileName(self, "Сохранить как", "", "PNG Файл (*.png)")
         if file_name:
-            self.parent().save_image(file_name, "png", exclude_menu=True)
+            self.save_image(file_name, "png")
 
-    def on_file_menu_triggered(self):
-        self.save_as_menu.show()
+    def on_zoom_changed(self, value):
+        self.zoom_level = value
+        if self.zoom_level == 100:
+            self.offset = self.original_offset
+        self.update()
+
+    def adjust_mouse_position(self, pos):
+        """
+        Выровнять позицию курсора при зуме
+        """
+        adjusted_pos = QPoint(
+            int((pos.x() - self.offset.x()) / (self.zoom_level / 100.0)),
+            int((pos.y() - self.offset.y()) / (self.zoom_level / 100.0))
+        )
+        return adjusted_pos
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = PaintWidget()
-    widget.resize(800, 600)  
+    widget.resize(800, 600)
     widget.show()
     sys.exit(app.exec_())
